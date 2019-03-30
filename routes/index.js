a755de3287827f75fde8df63a566a36176988135
Manuel Lang <manuellang183@gmail.com>
2019-03-30 14:14:55 +0100

    merge

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require("fs");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var content = fs.readFileSync("public/data/characters.json");
+  var characters = JSON.parse(content);
+  res.render('index', { title: 'Express', characters : characters });
 });
 
 router.get('/game', function(req, res, next) {
