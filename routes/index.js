dbb788dc2bf29ea872d22affad365b4bab0d684e
Manuel Lang <manuellang183@gmail.com>
2019-03-30 22:25:19 +0100

    icon and title

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var content = fs.readFileSync("public/data/characters.json");
   var characters = JSON.parse(content);
-  res.render('index', { title: 'Express', characters : characters });
+  res.render('index', { title: 'This Game Rocks!', characters : characters });
 });
 
 router.get('/game/:character_id', function(req, res, next) {
