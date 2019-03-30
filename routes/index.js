5cd5d345399acbb0989515b6e0fd6c69136046dd
Manuel Lang <manuellang183@gmail.com>
2019-03-30 23:13:25 +0100

    final pages

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get('/final/:balance', function(req, res, next) {
   if (balance > 1000000) {
     res.render('success', { balance: balance });
   }
-  else if(balance > 1000000) {
+  else if(balance > 10000) {
     res.render('okay', { balance: balance });
   }
   else {
