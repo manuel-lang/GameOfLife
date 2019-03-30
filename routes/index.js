e6c6b4e814df095c80e012d0354b030480facd7f
Manuel Lang <manuellang183@gmail.com>
2019-03-30 14:46:55 +0100

    data and routes

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,17 @@ var express = require('express');
 var fs = require("fs");
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   var content = fs.readFileSync("public/data/characters.json");
   var characters = JSON.parse(content);
   res.render('index', { title: 'Express', characters : characters });
 });
 
-router.get('/game', function(req, res, next) {
-  res.render('game', { character: req.query.id });
+router.get('/game/:character_id', function(req, res, next) {
+  var content = fs.readFileSync("public/data/questions.json");
+  var questions = JSON.parse(content);
+  var q = _.where(questions, {character: req.params.character_id});
+  res.render('game', { questions: q });
 });
 
 module.exports = router;
