300590a66978084bbadc78bdf6f58988a215bee1
Manuel Lang <manuellang183@gmail.com>
2019-03-31 12:13:35 +0200

    final pages, logic, fireworks, styles

--- routes/index.js
+++ routes/index.js
@@ -22,10 +22,10 @@ router.get('/game/:character_id', function(req, res, next) {
 
 router.get('/final/:balance', function(req, res, next) {
   let balance = req.params.balance;
-  if (balance > 1000000) {
+  if (balance >= 10000) {
     res.render('success', { balance: balance });
   }
-  else if(balance > 10000) {
+  else if(balance >= 1000) {
     res.render('okay', { balance: balance });
   }
   else {
